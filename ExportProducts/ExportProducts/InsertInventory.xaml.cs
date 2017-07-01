d12136430fa252c7cd3159c9ded906e985a64e72
Luiskar <luiskar009@gmail.com>
2017-07-01 19:35:37 +0200

    Added filter to InsertInventory.

--- ExportProducts/ExportProducts/InsertInventory.xaml.cs
+++ ExportProducts/ExportProducts/InsertInventory.xaml.cs
@@ -33,15 +33,124 @@ namespace ExportProducts
         {
             InitializeComponent();
 
-            // Look for products in the database and fill it in the comboBox
-            using (MySqlConnection connOrigen = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlDB"].ConnectionString))
+            articuloBoxMB.Items.Clear();
+            articuloBoxMB.SelectedIndex = articuloBoxMB.Items.Add("-- Selecione el producto de Odacash --");
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString.ToString()))
             {
-                MySqlCommand cmd = new MySqlCommand("SELECT Producto, id_product, id_product_attribute, Articulo, Baja FROM InventarioTablas ORDER BY Producto", connOrigen);
-                connOrigen.Open();
+                SqlCommand cmd = new SqlCommand("SELECT DescripcionCorta FROM VI_prueba_art ORDER BY DescripcionCorta;", conn);
+                conn.Open();
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        articuloBoxMB.Items.Add(rdr[0].ToString());
+                    }
+                }
+            }
+            idProductAttributeBox.Items.Clear();
+            idProductAttributeBox.SelectedIndex = idProductAttributeBox.Items.Add("-- Selecione un Atributo --");
+            productsBoxMB.Items.Clear();
+            productsBoxMB.SelectedIndex = productsBoxMB.Items.Add("-- Selecione el producto de Prestashop --");
+            using (MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlDB"].ConnectionString.ToString()))
+            {
+                MySqlCommand cmd2 = new MySqlCommand("SELECT DISTINCT name FROM ps_product_lang ORDER BY name", conn);
+                conn.Open();
+                using (MySqlDataReader rdr = cmd2.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        productsBoxMB.Items.Add(rdr[0].ToString());
+                    }
+                }
+
+                // Look for products in the database and fill it in the comboBox
+                MySqlCommand cmd = new MySqlCommand("SELECT Producto, id_product, id_product_attribute, Articulo, Baja FROM InventarioTablas ORDER BY Producto", conn);
                 sda = new MySqlDataAdapter(cmd);
                 dt = new DataTable("InventarioTablas");
                 sda.Fill(dt);
                 tablaSQL.ItemsSource = dt.DefaultView;
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        ///                                                                                            ///                                                                                         
+        ///         Updates the Prestashop ID when change the selected product in the comboBox         ///               
+        ///                                                                                            ///                                    
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        protected void productsBoxMB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (productsBoxMB.SelectedItem.ToString() == "-- Selecione el producto de Prestashop --")
+                return;
+            using (MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlDB"].ConnectionString.ToString()))
+            {
+                MySqlCommand cmd = new MySqlCommand($"SELECT id_product FROM ps_product_lang WHERE name = '{productsBoxMB.SelectedItem.ToString()}';", conn);
+                conn.Open();
+                using (MySqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    rdr.Read();
+                    boxIdProductMB.Text = rdr[0].ToString();
+                }
+                MySqlCommand cmd2 = new MySqlCommand($"SELECT id_product_attribute FROM ps_product_attribute WHERE id_product = '{boxIdProductMB.Text}';", conn);
+                using (MySqlDataReader rdr = cmd2.ExecuteReader())
+                {
+                    if (rdr.HasRows)
+                    {
+                        idProductAttributeBox.Items.Clear();
+                        idProductAttributeBox.SelectedIndex = idProductAttributeBox.Items.Add("-- Selecione un Atributo --");
+                        while (rdr.Read())
+                        {
+                            idProductAttributeBox.Items.Add(rdr[0].ToString());
+                        }
+                    }
+                    else
+                    {
+                        boxProductoMB.Text = "";
+                        productsBoxMB.SelectedItem = 0;
+                    }
+                }
+            }
+            boxProductoMB.Text = productsBoxMB.SelectedItem.ToString();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        ///                                                                                            ///                                                                                         
+        ///         Updates the Odacash ID when change the selected product in the comboBox            ///               
+        ///                                                                                            ///                                    
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        protected void articuloBoxMB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (articuloBoxMB.SelectedItem.ToString() == "-- Selecione el producto de Odacash --")
+                return;
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString.ToString()))
+            {
+                SqlCommand cmd = new SqlCommand($"SELECT Articulo FROM VI_prueba_art WHERE DescripcionCorta = '{articuloBoxMB.SelectedItem.ToString()}';", conn);
+                conn.Open();
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    rdr.Read();
+                    boxArticuloMB.Text = rdr[0].ToString();
+                }
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        ///                                                                                            ///                                                                                         
+        ///         Updates the attribute ID when change the selected product in the comboBox          ///               
+        ///                                                                                            ///                                    
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        protected void idProductAttributeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(idProductAttributeBox.HasItems)
+            {
+                if (idProductAttributeBox.SelectedItem.ToString() == "-- Selecione un Atributo --")
+                {
+                    boxAttributeMB.Text = "0";
+                    return;
+                }
+                boxAttributeMB.Text = idProductAttributeBox.SelectedItem.ToString();
             }
         }
 
@@ -149,6 +258,92 @@ namespace ExportProducts
                 }
             }
             MessageBox.Show("Se ha actualizado la Base de Datos", "Actualizado", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK, System.Windows.MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        ///                                                                                            ///                                               
+        ///                         Event to change focus when click outside                           ///               
+        ///                                                                                            ///                                    
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            Keyboard.ClearFocus();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        ///                                                                                            ///                                               
+        ///              Event tu update the Odacash comboBox when TextBox lost focus                  ///               
+        ///                                                                                            ///                                    
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void idProductMB_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            // If the textBox is empty do nothing
+            if (boxIdProductMB.Text == "")
+                return;
+            string producto = "";
+
+            // Look for the product name for the id inserted in the textbox
+            using (MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlDB"].ConnectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand($"SELECT name FROM ps_product_lang WHERE id_product = '{boxIdProductMB.Text}'", conn);
+                conn.Open();
+                using (MySqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    rdr.Read();
+                    // If the products exist save the name
+                    if (rdr.HasRows)
+                        producto = rdr[0].ToString();
+
+                    // If not exist select the index and finish the execution
+                    else
+                    {
+                        productsBoxMB.SelectedIndex = 0;
+                        boxProductoMB.Text = "";
+                        return;
+                    }
+                }
+            }
+            // Update the comboBox item selection with the product name
+            productsBoxMB.SelectedIndex = productsBoxMB.Items.IndexOf(producto);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        ///                                                                                            ///                                               
+        ///              Event tu update the Odacash comboBox when TextBox lost focus                  ///               
+        ///                                                                                            ///                                    
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void idOdacashMB_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            // If the textBox is empty do nothing
+            if (boxArticuloMB.Text == "")
+                return;
+            string articulo = "";
+
+            // Look for the product name for the id inserted in the textbox
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand($"SELECT DescripcionCorta FROM VI_prueba_art WHERE Articulo = '{boxArticuloMB.Text}'", conn);
+                conn.Open();
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    rdr.Read();
+                    // If the products exist save the name
+                    if (rdr.HasRows)
+                        articulo = rdr[0].ToString();
+
+                    // If not exist select the index and finish the execution
+                    else
+                    {
+                        articuloBoxMB.SelectedIndex = 0;
+                        return;
+                    }
+                }
+            }
+            // Update the comboBox item selection with the product name
+            articuloBoxMB.SelectedIndex = articuloBoxMB.Items.IndexOf(articulo);
         }
 
         //////////////////////////////////////////////////////////////////////////////////////////////////
